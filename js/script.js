36a7ed9aa4400e1f681a0200e0d31f8025af1bcf
Gabriel Lima Souza <gabriel.souza@sequoia.local>
2022-04-19 13:09:25 -0300

    Atualizado em 19-04

--- js/script.js
+++ js/script.js
@@ -3,11 +3,9 @@ import cartaoCadastro from "./cartao-cadastro.js";
 import { Validacao } from "./validacao.js";
 import Transacao from "./transacao.js";
 import limiteCartao from "./limite.js";
-import Pagamento from "./pagamento.js";
 
 Validacao(".btn-cadastrar", ".box-cadastro form [required]");
 Validacao(".btn-adicionar", "[data-formulario='transacao'] [required]");
-Validacao(".btn-confirmar", "#pagamento[required]");
 
 const app = App();
 app.init();
@@ -20,6 +18,3 @@ transacao.init();
 
 const limite = limiteCartao();
 limite.init();
-
-const pagamento = Pagamento();
-pagamento.init();
